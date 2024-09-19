9007c53eb0f9d2f2b5f716cac315ccdcf7d40d87
Maksym Fedorchuk <maxeffect.apollo@gmail.com>
2024-09-19 22:21:16 +0300

    spaces

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -7,7 +7,9 @@ export const Person = ({ person }) => {
   return (
     <div className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
+
       {age && <p className="Person__age">{`I am ${age}`}</p>}
+
       {isMarried ? (
         <p className="Person__partner">{`${partnerName} is my ${spouse}`}</p>
       ) : (
